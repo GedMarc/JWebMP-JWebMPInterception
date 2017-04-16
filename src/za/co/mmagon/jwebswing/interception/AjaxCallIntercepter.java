7b0ae35a803906d5bed8ba6c893227dd28afe0c3
Marc Magon <ged_marc@hotmail.com>
2017-04-16 15:27:08 +0200

    JWebSwing Interception Remote Library

---
+++ src/za/co/mmagon/jwebswing/interception/AjaxCallIntercepter.java
@@ -0,0 +1,11 @@
+package za.co.mmagon.jwebswing.interception;
+
+/**
+ *
+ * @author Marc Magon
+ * @since 10 Apr 2017
+ */
+public interface AjaxCallIntercepter extends DefaultIntercepter
+{
+
+}
---
+++ src/za/co/mmagon/jwebswing/interception/DefaultIntercepter.java
@@ -0,0 +1,16 @@
+package za.co.mmagon.jwebswing.interception;
+
+/**
+ *
+ * @author Marc Magon
+ * @since 10 Apr 2017
+ */
+public interface DefaultIntercepter
+{
+
+    /**
+     * Intercepts a call from this method
+     */
+    public void intercept();
+
+}
