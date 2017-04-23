ec7f3da14ee7c190eecfa0a0797b0cdec677746f
Marc Magon <ged_marc@hotmail.com>
2017-04-23 17:58:09 +0200

    add test folder for sonar

---
+++ test/za/co/mmagon/jwebswing/interception/AjaxCallIntercepterTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.co.mmagon.jwebswing.interception;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Marc Magon
+ */
+public class AjaxCallIntercepterTest
+{
+
+    public AjaxCallIntercepterTest()
+    {
+    }
+
+    @Test
+    public void testSomeMethod()
+    {
+    }
+
+}
