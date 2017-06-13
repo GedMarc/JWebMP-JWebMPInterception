9f2cd58c98c845b3cfb03f77142f85ec6c2d77ee
Marc Magon <ged_marc@hotmail.com>
2017-06-14 00:59:05 +0200

    Licensing Update

--- src/za/co/mmagon/jwebswing/interception/DefaultIntercepter.java
+++ src/za/co/mmagon/jwebswing/interception/DefaultIntercepter.java
@@ -1,6 +1,23 @@
+/* 
+ * Copyright (C) 2017 Marc Magon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package za.co.mmagon.jwebswing.interception;
 
 /**
+ * Intercepts all calls at all points
  *
  * @author Marc Magon
  * @since 10 Apr 2017
@@ -13,4 +30,10 @@ public interface DefaultIntercepter
      */
     public void intercept();
 
+    /**
+     * Sorts accordingly, default applied 100
+     *
+     * @return
+     */
+    public Integer sortOrder();
 }
