23f41d53c688ae907db30617a0430775f512d83a
GedMarc <ged_marc@hotmail.com>
2018-07-19 04:36:51 +0200

    JDK 10 Dependency Updates

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,6 +1,11 @@
+import com.jwebmp.guicedinjection.interfaces.IGuiceConfigurator;
+import com.jwebmp.interception.IGuiceConfiguration;
+
 module com.jwebmp.interception{
 	requires com.jwebmp.guicedinjection;
 
 	exports com.jwebmp.interception;
 	exports com.jwebmp.interception.interfaces;
+
+	provides IGuiceConfigurator with IGuiceConfiguration;
 }
