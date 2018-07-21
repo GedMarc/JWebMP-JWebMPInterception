bac2ffcb01b5822e9fe9a0e970aa0717df19d329
GedMarc <ged_marc@hotmail.com>
2018-07-22 01:28:19 +0200

    JDK 10 and Builds

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -2,6 +2,10 @@ import com.jwebmp.interception.JWebMPGuiceContextConfig;
 
 module com.jwebmp.interception{
 	requires com.jwebmp.guicedinjection;
+	requires aopalliance;
+	requires com.jwebmp.logmaster;
+	requires java.logging;
+	requires com.google.guice;
 
 	exports com.jwebmp.interception;
 	exports com.jwebmp.interception.interfaces;
